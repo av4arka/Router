ea83bdcbad5568d4447be4cd19c95908c8066312
av4arka <tima.larionoww@gmail.com>
2019-03-01 16:02:39 +0200

    update

--- ip_v4_address.py
+++ ip_v4_address.py
@@ -3,7 +3,7 @@ from exceptions import InvalidIPv4Address
 class IPv4Address:
 
     def valid_address(self, address):
-        if type(address) is int:
+        if isinstance(address, int):
             max = 4294967295
 
             if address > max or address < 0:
@@ -15,19 +15,17 @@ class IPv4Address:
             max = 4
             index = 0
 
-            points = address.split('.')
-
-            if len(points) > max:
+            octet = address.split('.')
+            if len(octet) > max:
                 return False
 
             while index < max:
-                if int(points[index]) < 0 or int(points[index]) > 255:
+                if int(octet[index]) < 0 or int(octet[index]) > 255:
                     return False
-                if points[index][0] == '0' and len(points[index]) != 1:
+                if octet[index][0] == '0' and len(octet[index]) != 1:
                     return False
-                if points[index][0] == '-':
+                if octet[index][0] == '-':
                     return False
-
                 index += 1
 
         except Exception:
@@ -43,38 +41,35 @@ class IPv4Address:
         pow = 3
         num = 0
 
-        if type(self._ip) is not str:
-            return int(self._ip)
+        if isinstance(self._ip, int):
+            return self._ip
 
-        for quad in self._ip.split('.'):
-            num += (int(quad) * (256 ** pow))
+        for octet in self._ip.split('.'):
+            num += (int(octet) * (256 ** pow))
             pow -= 1
         return num
 
     def convert_number_to_ip(self):
         div = 16777216
-        index = 0
         address = [0, 0, 0, 0]
-        point = '.'
         ip = self._ip
 
-        if type(self._ip) is str:
+        if isinstance(self._ip, str):
             return self._ip
 
-        while index < 4:
+        for index in range(4):
             num = int(ip / div)
             ip -= div * num
-            address[index] = str(num)
             div /= 256
-            index += 1
+            address[index] = str(num)
 
-        return point.join(address)
+        return '.'.join(address)
 
     def less_than(self, address):
         if self.valid_address(address):
             address2 = IPv4Address(address)
-
             return self.convert_ip_to_number() < address2.convert_ip_to_number()
+        raise InvalidIPv4Address('Invalid address!')
 
     def greater_than(self, address):
         return not self.less_than(address)
@@ -84,9 +79,25 @@ class IPv4Address:
             address2 = IPv4Address(address)
 
             return self.convert_ip_to_number() == address2.convert_ip_to_number()
+        raise InvalidIPv4Address('Invalid address!')
 
     def to_string(self):
         return self.convert_number_to_ip()
 
     def to_long(self):
         return self.convert_ip_to_number()
+
+if __name__ == '__main__':
+    address = IPv4Address('127.0.0.1')
+    print(address.to_string())
+    print(address.to_long())
+
+    address = IPv4Address(2130706433)
+    print(address.to_string())
+    print(address.to_long())
+
+    print(address.equals('127.0.1.1'))
+    print(address.equals(2130706433))
+    print(address.greater_than('10.10.1.1'))
+    print(address.greater_than(543070343))
+    print(address.less_than('25.38.38.5'))
